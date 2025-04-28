a6f7639aefe30483ed016ada1a5b53b872b61931
VTarabukhin <vladimir.tarabukhin@gmail.com>
2025-04-28 18:02:24 -0400

    Lesson 12
    Made gradebook html and Javascript files for lesson 12

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradedata() {
+    console.log("Fetching grade data...");
+}
+
+function populateGradebook(data) {
+    console.log("populating gradebook with data:", data);
+}
+
+const gradedata = fetchGradedata();
+populateGradebook(gradeData);
