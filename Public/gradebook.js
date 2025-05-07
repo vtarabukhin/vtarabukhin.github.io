5b222d81cf385dd98e3bce2a23d8e0090d67a151
VTarabukhin <vladimir.tarabukhin@gmail.com>
2025-05-06 20:34:10 -0400

    Final Project
    Final Project

--- Public/gradebook.js
+++ Public/gradebook.js
@@ -1,10 +1,39 @@
-function fetchGradedata() {
+console.log("JavaScript is loaded");
+
+function fetchGradeData() {
     console.log("Fetching grade data...");
+    let xhr = new XMLHttpRequest();
+    let apiRoute = "/api/grades";
+    xhr.onreadystatechange = function () {
+        let results;
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status !== 200) {
+                console.error('Could not get grades.'); //it kept bringing up errors throughout the entire code telling me error at #:# until i changed this line
+                console.error(`Status: ${xhr.status}`);
+            }
+            populateGradebook(JSON.parse(xhr.responseText));
+        }
+    }.bind(this);
+    xhr.open("get", apiRoute, true);
+    xhr.send();
 }
 
-function populateGradebook(data) {
-    console.log("populating gradebook with data:", data);
-}
-
-const gradedata = fetchGradedata();
-populateGradebook(gradeData);
+    function populateGradebook(data) {
+        console.log("populating gradebook with data:", data);
+        let tableElm = document.getElementById("gradebook");
+        data.forEach(function (assignment) {
+            let row = document.createElement("tr");
+            let columns = [];
+            columns.name = document.createElement('td');
+            columns.name.appendChild(
+                document.createTextNode(assignment.last_name + "," + assignment.first_name)
+            );
+            columns.grade = document.createElement('td');
+            columns.grade.appendChild(
+                document.createTextNode(assignment.total_grade)
+            );
+            row.appendChild(columns.name);
+            row.appendChild(columns.grade);
+            tableElm.appendChild(row);
+        });
+    }  
